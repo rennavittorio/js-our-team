7b54c6ec3cee11127d8d7e9d69b526e64ac06c18
rennavittorio <renna.vittorio@gmail.com>
2023-02-14 14:34:31 +0100

    MLS 0 completed

---
+++ js/app.js
@@ -0,0 +1,63 @@
+//prototype object
+const person = {
+
+    firstName: 'name',
+    lastName: 'surname',
+    jobRole: 'job',
+    profileImg: './img',
+
+}
+
+let companyEmployees = [
+
+    {
+        firstName: 'Wayne',
+        lastName: 'Barnett',
+        jobRole: 'Founder & CEO',
+        profileImg: './assets/wayne-barnett-founder-ceo.jpg',
+    
+    },
+
+    {
+        firstName: 'Angela',
+        lastName: 'Caroll',
+        jobRole: 'Chief Editor',
+        profileImg: './assets/angela-caroll-chief-editor.jpg',
+    
+    },
+
+    {
+        firstName: 'Walter',
+        lastName: 'Gordon',
+        jobRole: 'Office Manager',
+        profileImg: './assets/walter-gordon-office-manager.jpg',
+    
+    },
+
+    {
+        firstName: 'Angela',
+        lastName: 'Lopez',
+        jobRole: 'Social Media Manager',
+        profileImg: './assets/angela-lopez-social-media-manager.jpg',
+    
+    },
+
+    {
+        firstName: 'Scott',
+        lastName: 'Estrada',
+        jobRole: 'Developer',
+        profileImg: './assets/scott-estrada-developer.jpg',
+    
+    },
+
+    {
+        firstName: 'Barbara',
+        lastName: 'Ramos',
+        jobRole: 'Graphic Designer',
+        profileImg: './assets/barbara-ramos-graphic-designer.jpg',
+    
+    },
+
+]
+
+console.log('company team:', companyEmployees);
